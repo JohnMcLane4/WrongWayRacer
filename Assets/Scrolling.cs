3b97c3410caa1f72fb44ab8e95840daeb646713b
Respaekt <33717665+JohnMcLane4@users.noreply.github.com>
2019-02-13 11:30:19 +0100

    Initial Commit

---
+++ Assets/Scrolling.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+
+    [SerializeField]
+    private float scrollSpeed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, Time.time * 0.1f * scrollSpeed);
+    }
+}
